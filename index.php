d4bb30143564b5e20cc265ca3806489c0a1aac87
omdeongs <89972986+omdeongs@users.noreply.github.com>
2021-09-02 17:53:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="refresh" content="900" />
+  <meta http-equiv="X-UA-Compatible" content="IE=9" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no,minimal-ui" />
+
+  <!-- primary information-->
+  <title>Beranda | Bintang Fotocopy</title>
+  <meta name="description" content="Beranda | Bintang Fotocopy" />
+  <meta name="keywords" content="Beranda, Bintang Fotocopy, indonesia" />
+
+  <!-- ie fix for html5 tags-->
+  <!--[if lt IE 9]><script src='http://html5shiv.googlecode.com/svn/trunk/html5.js'></script><![endif]-->
+
+  <!-- author-->
+  <meta name="author" content="Bintang Fotocopy" />
+  <meta name="copyright" content="2021 Bintang Fotocopy. All Right Reserved" />
+
+  <!-- user agent crawler-->
+  <meta name="robots" content="index, follow" />
+  <meta name="googlebot" content="index, follow" />
+  <meta name="googlebot-news" content="index, follow" />
+  <meta name="msnbot" content="index, follow" />
+  <meta name="webcrawlers" content="index, follow" />
+  <meta name="spiders" content="index, follow" />
+
+  <!-- canonical-->
+  <link rel="canonical" href="https://www.bintang-fotocopy.com" />
+
+  <!-- og facebook general-->
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Beranda | Bintang Fotocopy" />
+  <meta property="og:description" content="Beranda | Bintang Fotocopy" />
+  <meta property="og:url" content="https://www.bintang-fotocopy.com" />
+  <meta property="og:site_name" content="bintang-fotocopy.com" />
+  <meta property="og:image" content="assets/img/default/og-facebook.jpg" />
+  <meta property="og:image:type" content="image/jpeg" />
+
+  <!-- twitter card-->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Beranda | Bintang Fotocopy" />
+  <meta name="twitter:description" content="Beranda | Bintang Fotocopy" />
+  <meta name="twitter:site" content="@bintang-fotocopy" />
+  <meta name="twitter:site:id" content="@bintang-fotocopy" />
+  <meta name="twitter:domain" content="bintang-fotocopy.com" />
+  <meta name="twitter:image:src" content="assets/img/default/twitter-card.jpg" />
+  <meta name="twitter:creator" content="@bintang-fotocopy" />
+
+  <!-- ============ icon ============-->
+  <!-- web favicon-->
+  <link rel="shortcut icon" href="assets/img/homescreen/favicon.ico" />
+
+  <!-- android add to home screen-->
+  <link rel="manifest" href="assets/js/data/manifest.json" />
+  <meta name="mobile-web-app-capable" content="yes" />
+  <meta name="theme-color" content="#fa759f" />
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/homescreen/favicon-16x16.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/homescreen/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/img/homescreen/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="144x144" href="assets/img/homescreen/android-icon-144x144.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/img/homescreen/android-icon-192x192.png" />
+
+  <!-- windows microsoft-->
+  <meta name="msapplication-TileColor" content="#fa759f" />
+  <meta name="msapplication-TileImage" content="assets/img/homescreen/ms-icon-144x144.png" />
+
+  <!-- apple add to home screen-->
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="#fa759f" />
+  <link rel="apple-touch-icon" href="assets/img/homescreen/apple-icon.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/img/homescreen/apple-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/img/homescreen/apple-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/homescreen/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/homescreen/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/img/homescreen/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/img/homescreen/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/img/homescreen/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/img/homescreen/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/homescreen/apple-icon-180x180.png" />
+  <link rel="apple-touch-startup-image" href="assets/img/homescreen/apple-icon.png" />
+
+  <!-- style-->
+  <link rel="stylesheet" href="assets/css/app.min.css" />
+
+</head>
+
+<body class="hold-transition">
+  <!--header-->
+  <div class="header">
+    <div class="container">
+      <div class="header__content">
+        <!--header-logo--><a class="header__logo" href="index.html" style="true">
+          Home
+        </a>
+        <!--burger-menu--><button class="burger-menu js-nav" type="button">
+          <span class="burger-menu__bar"></span>
+
+          <span class="burger-menu__bar"></span>
+
+          <span class="burger-menu__bar"></span>
+        </button>
+        <!--header-menu-->
+        <div class="header__menu">
+          <ul class="header__nav">
+            <li class="header__nav__item">
+              <a class="header__nav__link" href="#about">about</a>
+            </li>
+            <li class="header__nav__item">
+              <a class="header__nav__link" href="#portfolio">portfolio</a>
+            </li>
+            <li class="header__nav__item">
+              <a class="header__nav__link" href="#blog">blog</a>
+            </li>
+            <li class="header__nav__item">
+              <a class="header__nav__link" href="#team">team</a>
+            </li>
+            <li class="header__nav__item">
+              <a class="header__nav__link" href="#contact">contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main-site main-site--hide js-main-site">
+    <!--hero-banner-->
+    <div class="hero-banner js-hero-banner" id="about">
+      <!--hero-banner-item-->
+      <div class="hero-banner__item">
+        <div class="hero-banner__img">
+          <img class="hero-banner__img__el" src="assets/img/dummy/bg-08.jpg" alt="Angga Deongs" />
+        </div>
+        <div class="hero-banner__txt">
+          <h2 class="hero-banner__title">Angga Deongs</h2>
+          <h4 class="hero-banner__desc">Lulusan Sekolah Menengah Kejuruan, Bekerja Sebagai Middle Front-end Developer disalah satu perusahan digital agency di daerah jakarta selatan, Ingin menambah ilmu melalui pelatihan Hacktiv8</h4>
+        </div>
+      </div>
+    </div>
+    <!--portfolio-->
+    <div class="portfolio" id="portfolio">
+      <div class="container">
+        <h2 class="portfolio__title title--section">Portfolio</h2>
+        <p class="portfolio__description text--description">Salah satu dari banyak website yang telah dibikin</p>
+        <!--portfolio-list-->
+        <div class="portfolio__list">
+          <!--fotocopy-summary-item-->
+          <div class="fotocopy-summary__item">
+            <div class="fotocopy-summary__image"><img src="assets/img/dummy/bg-1.png" /></div>
+          </div>
+          <!--fotocopy-summary-item-->
+          <div class="fotocopy-summary__item">
+            <div class="fotocopy-summary__image"><img src="assets/img/dummy/bg-2.png" /></div>
+          </div>
+        </div><a class="portfolio__link btn btn--black" href="#">Selengkapnya</a>
+      </div>
+    </div>
+    <!--blog-->
+    <div class="blog" id="blog">
+      <div class="container">
+        <div class="blog__wrapper">
+          <div class="blog__text">
+            <h2 class="blog__title title--section">Blog</h2>
+            <p class="blog__description text--description">Berikut adalah contoh dummy blog</p>
+          </div>
+          <!--blog-list-->
+          <div class="blog__list owl-carousel js-carousel-jilid-summary">
+            <!--card-primary-item-->
+            <div class="card-primary__item">
+              <div class="card-primary__item__image"><img src="assets/img/dummy/bg-01.jpg" alt="blog 1" /></div>
+              <div class="card-primary__item__text">
+                <h3 class="card-primary__item__title">blog 1</h3>
+              </div><a class="card-primary__item__link bfc--link" href="#">blog 1</a>
+            </div>
+            <!--card-primary-item-->
+            <div class="card-primary__item">
+              <div class="card-primary__item__image"><img src="assets/img/dummy/bg-02.jpg" alt="blog 2" /></div>
+              <div class="card-primary__item__text">
+                <h3 class="card-primary__item__title">blog 2</h3>
+              </div><a class="card-primary__item__link bfc--link" href="#">blog 2</a>
+            </div>
+            <!--card-primary-item-->
+            <div class="card-primary__item">
+              <div class="card-primary__item__image"><img src="assets/img/dummy/bg-03.jpg" alt="blog 3" /></div>
+              <div class="card-primary__item__text">
+                <h3 class="card-primary__item__title">blog 3</h3>
+              </div><a class="card-primary__item__link bfc--link" href="#">blog 3</a>
+            </div>
+            <!--card-primary-item-->
+            <div class="card-primary__item">
+              <div class="card-primary__item__image"><img src="assets/img/dummy/bg-04.jpg" alt="blog 4" /></div>
+              <div class="card-primary__item__text">
+                <h3 class="card-primary__item__title">blog 4</h3>
+              </div><a class="card-primary__item__link bfc--link" href="#">blog 4</a>
+            </div>
+            <!--card-primary-item-->
+            <div class="card-primary__item">
+              <div class="card-primary__item__image"><img src="assets/img/dummy/bg-01.jpg" alt="blog 5" /></div>
+              <div class="card-primary__item__text">
+                <h3 class="card-primary__item__title">blog 5</h3>
+              </div><a class="card-primary__item__link bfc--link" href="#">blog 5</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--team-summary-->
+    <div class="team-summary" id="team">
+      <div class="container">
+        <h2 class="team-summary__title title--section">Tim Kami</h2>
+        <!--team-summary-list-->
+        <div class="team-summary__list">
+          <!--team-summary-item-->
+          <div class="team-summary__item">
+            <div class="team-summary__box">
+              <div class="team-summary__image"><img class="team-summary__image__el" src="assets/img/dummy/tim-1.png" alt="Angga" /></div>
+              <div class="team-summary__text">
+                <h4 class="team-summary__name">Angga<span class="team-summary__last-name">Deongs</span></h4>
+                <h5 class="team-summary__position">Owner</h5>
+              </div>
+            </div>
+          </div>
+          <!--team-summary-item-->
+          <div class="team-summary__item">
+            <div class="team-summary__box">
+              <div class="team-summary__image"><img class="team-summary__image__el" src="assets/img/dummy/tim-1.png" alt="Angga" /></div>
+              <div class="team-summary__text">
+                <h4 class="team-summary__name">Angga<span class="team-summary__last-name">Deongs</span></h4>
+                <h5 class="team-summary__position">Web Developer</h5>
+              </div>
+            </div>
+          </div>
+          <!--team-summary-item-->
+          <div class="team-summary__item">
+            <div class="team-summary__box">
+              <div class="team-summary__image"><img class="team-summary__image__el" src="assets/img/dummy/tim-1.png" alt="Angga" /></div>
+              <div class="team-summary__text">
+                <h4 class="team-summary__name">Angga<span class="team-summary__last-name">Deongs</span></h4>
+                <h5 class="team-summary__position">Web Developer</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--contact-->
+    <div class="contact">
+      <div class="contact__image"><img class="contact__image__el" src="assets/img/dummy/bg-07.jpg" alt="Contact Person: 085222433981" /></div>
+      <h3 class="contact__text">Contact Person: 085222433981</h3>
+    </div>
+    <!--whatsapp--><a class="widget-wa" href="#"><img class="widget-wa__el" src="assets/img/icons/icon-whatsapp.png" alt="whatsapp" /></a>
+  </div>
+  <!--footer-->
+  <div class="footer">
+    <div class="container">
+      <div class="footer__top">
+        <div class="footer__content">
+          <div class="footer__about">
+            <div class="footer__about__wrapper"><a class="footer__logo" href="index.html">Logo</a>
+              <p class="footer__description">Angga Deongs Middle Frontend Developer</p>
+            </div>
+          </div>
+          <div class="footer__menu">
+            <h3 class="footer__title">Jam Buka</h3>
+            <ul class="footer__nav">
+              <li class="footer__nav__item">Senin : 08.00–20.00</li>
+              <li class="footer__nav__item">Selasa : 08.00–20.00</li>
+              <li class="footer__nav__item">Rabu : 08.00–20.00</li>
+              <li class="footer__nav__item">Kamis : 08.00–20.00</li>
+              <li class="footer__nav__item">Jumat : 08.00–20.00</li>
+              <li class="footer__nav__item">Sabtu : 08.00–20.00</li>
+              <li class="footer__nav__item">Minggu : Tutup</li>
+            </ul>
+          </div>
+          <div class="footer__contact">
+            <div class="footer__contact__item"><img class="footer__contact__icon" src="assets/img/icons/icon-location.svg" alt="location" />
+              <div class="footer__contact__txt">
+                <p>Jl. Cut Nyak Dhien, Cijoho, Kec. Kuningan, Kabupaten Kuningan, Jawa Barat 45513, Indonesia</p>
+              </div>
+            </div><a class="footer__contact__item" href="#">anggadeongs11@gmail.com<img class="footer__contact__icon" src="assets/img/icons/icon-email.svg" alt="email" /></a>
+            <div class="footer__contact__item"><img class="footer__contact__icon" src="assets/img/icons/icon-phone.svg" alt="phone" />
+              <div class="footer__contact__txt">
+                <p>0811-1111-8109</p>
+                <p>0852-2243-3981</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer__bottom">
+        <p class="footer__bottom__copyright">Copyright &copy; 2021 Angga Deongs. All Rights Reserved</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- script-->
+  <script src="assets/js/vendor.min.js"></script>
+  <script src="assets/js/app.min.js"></script>
+
+</body>
+
+</html>
